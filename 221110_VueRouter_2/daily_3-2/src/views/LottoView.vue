ac66d5eeab1d7de3d9fcf9c42f32ca64997c142f
김규리 <qri1114@gmail.com>
2022-11-10 17:57:12 +0900

    데일리 과제 3-2

---
+++ 221110_VueRouter_2/daily_3-2/src/views/LottoView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="lotto">
+    <h1>일확천금</h1>
+    <button
+      @click="getRandomNums"
+    >오늘의 랜덤 번호는!!</button>
+    <p>
+      <span v-for="(num, idx) in luckyNums" :key="idx">  {{ num }}  </span>
+    </p>
+  </div>
+</template>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.min.js" integrity="sha512-WFN04846sdKMIP5LKNphMaWzU7YpMyCU245etK3g/2ARYbPK9Ub18eG+ljU96qKRCWh+quCY7yefSmlkQw1ANQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script>
+const _ = require('lodash')
+export default {
+  name: 'LottoView',
+  data(){
+    return{
+      nums: new Array(45).fill(1).map((v, i)=>v+i),
+      luckyNums: null,
+    }
+  },
+  methods: {
+    getRandomNums(){
+      console.log(this.nums);
+      this.luckyNums = _.sampleSize(this.nums, 6).sort((a, b)=>a-b);
+    }
+  }
+}
+</script>
---
+++ 221110_VueRouter_2/daily_3-2/src/views/LunchView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>점심메뉴</h1>
+    <button
+      @click="pickMenu"
+    >Pick Menu🍱</button>
+    <p>{{ menu }}</p>
+    <h2>로또 뽑으러 가자궁~</h2>
+    <button
+      @click="pickLotto"
+    >Pick Lotto</button>
+  </div>
+</template>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.min.js" integrity="sha512-WFN04846sdKMIP5LKNphMaWzU7YpMyCU245etK3g/2ARYbPK9Ub18eG+ljU96qKRCWh+quCY7yefSmlkQw1ANQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script>
+const _ = require('lodash')
+
+export default {
+  name: 'LunchView',
+  data(){
+    return{
+      menus: ['쌀국수', '지하철(1호선)', '규카츠ㅋ', '명인만두', '라멘', '햄벅어', '국뚜', '뜅~', '우동명가기리야마본진', '한솥'],
+      menu: '.',
+    }
+  },
+  methods: {
+    pickMenu(){
+      this.menu = _.sample(this.menus)
+      console.log();
+    },
+    pickLotto(){
+      this.$router.push({ name: 'lotto' })
+    } 
+  }
+}
+</script>
+
+<style>
+
+</style>
